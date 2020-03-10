bbfe3c6214f95e4a76ae8a46b690cfd2f9424c01
RoiArthurB <contact@arthurbrugiere.fr>
2020-03-10 11:53:07 +0700

    Implementing Singleton design pattern

--- src/OpenMole.php
+++ src/OpenMole.php
@@ -1,4 +1,6 @@
-<?php namespace RoiArthurB\OpenMole;
+<?php 
+
+namespace RoiArthurB\OpenMole;
 
 /**
 *  A sample class
@@ -6,24 +8,72 @@
 *  Use this section to define what this class is doing, the PHPDocumentator will use this
 *  to automatically generate an API documentation using this information.
 *
-*  @author yourname
+*  @author RoiArthurB
 */
 class OpenMole{
 
-   /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-   private $m_SampleProperty = '';
+	/*
+			+========
+			|   SINGLETON   |
+                    ========+
+	 */
+
+	/**
+	 * Return instance of this class
+	 *
+	 * @static var Singleton $instance
+	 *
+	 * @return Singleton instance
+	 */
+	public static function getInstance() {
+		static $instance = null;
+		if (null === $instance) {
+				$instance = new static();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Protected construtor to prevent the creation of a new instance with the "new" keyword
+	 */
+	protected function __construct() {}
+
+	/**
+	 * Private clone function to prevent cloning the singleton instance
+	 *
+	 * @return void
+	 */
+	private function __clone() {}
+
+	/**
+	 * Private unserialize function to prevent cloning the singleton instance
+	 *
+	 * @return void
+	 */
+	private function __wakeup() {}
+
+
+	/*
+			+========
+			|      API      |
+                    ========+
+	 */
+
+	 /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
+	 private $m_SampleProperty = '';
  
-  /**
-  * Sample method 
-  *
-  * Always create a corresponding docblock for each method, describing what it is for,
-  * this helps the phpdocumentator to properly generator the documentation
-  *
-  * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
-  *
-  * @return string
-  */
-   public function method1($param1){
+	/**
+	* Sample method 
+	*
+	* Always create a corresponding docblock for each method, describing what it is for,
+	* this helps the phpdocumentator to properly generator the documentation
+	*
+	* @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
+	*
+	* @return string
+	*/
+	 public function method1($param1){
 			return "Hello World";
-   }
+	 }
 }
